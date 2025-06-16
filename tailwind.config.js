8d0de0883a46a1f45248f6ed20a8407bcfea32d1
Sarah Friday <sarah.k.friday.gr@dartmouth.edu>
2025-06-16 16:14:46 -0400

    fix responsiveness of people page

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,7 @@ let h2 = h3 * font_scale;
 let h1 = h2 * font_scale;
 
 let fontPrimary = "chakra petch";
-let fontPrimaryType = "serif";
+let fontPrimaryType = "sans-serif";
 let fontSecondary = "chakra petch";
 let fontSecondaryType = "sans-serif";
 
